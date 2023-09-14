5bcf7292ecfb0f8eaad24dba1ca4c3dee4813e31
cansukeles <zeynepcansuoral@gmail.com>
2023-09-14 15:49:22 +0300

    Vucut Kitle Indeksine Yorum

--- VucutKitleIndeksiHesaplayanProgram/src/Main.java
+++ VucutKitleIndeksiHesaplayanProgram/src/Main.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // boy ve kilo değişkenleri tanımlanır.
         int kilo;
         double boy, indeks;
 
         Scanner inp = new Scanner(System.in);
 
+        //kullanıcıdan inputlar alınır.
         System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz: ");
         boy = inp.nextDouble();
 
         System.out.print("Lütfen Kilonuzu Giriniz: ");
         kilo = inp.nextInt();
 
-        double in = kilo / (boy * boy);
+        //Indeks hesaplanır.
+        indeks = kilo / (boy * boy);
 
 
-        System.out.println("Vücut Kitle İndeksiniz: " + in);
+        System.out.println("Vücut Kitle İndeksiniz: " + indeks);
 
 
 

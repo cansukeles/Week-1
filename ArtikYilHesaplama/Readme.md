3cfaa0d58993f209223cc132a6a8c6a9fb1f6c3f
cansukeles <zeynepcansuoral@gmail.com>
2023-09-15 19:30:43 +0300

    Artik Yil Hesaplama

---
+++ ArtikYilHesaplama/Readme.md
@@ -0,0 +1,2 @@
+### Artık yıl hesaplama programı
+Kullanıcıdan **yıl** verisi alarak artık yıl olup olmadığını belirten program
---
+++ ArtikYilHesaplama/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Write the year: ");
+        year = input.nextInt();
+
+        if((year % 4 == 0) || (year % 400 == 0)){
+            System.out.print(year + " is a leap year");
+        }else {
+            System.out.print(year + " isn't a leap year");
+        }
+    }
+}
